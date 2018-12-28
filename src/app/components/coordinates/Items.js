dac9b3e30ae07df02268ea964450c936881db984
佐藤一輝 <i.tennis0210@gmail.com>
2018-12-28 17:46:08 +0900

    build環境の用意

--- src/app/components/coordinates/Items.js
+++ src/app/components/coordinates/Items.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Item from 'Item'
+import Item from './Item'
 
 const Items = props => {
   const { coordinates, ...otherProps } = props
