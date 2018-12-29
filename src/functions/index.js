099a96489866375de0eb522104bd5e7b11f3a21c
佐藤一輝 <i.tennis0210@gmail.com>
2018-12-29 17:33:12 +0900

    postをfunctionsで受け付ける

--- src/functions/index.js
+++ src/functions/index.js
@@ -7,5 +7,14 @@ const handle = app.getRequestHandler()
 
 exports.next = functions.https.onRequest((req, res) => {
   console.log('File: ' + req.originalUrl) // log the page.js file that is being requested
-  return app.prepare().then(() => handle(req, res))
+  return app.prepare().then(() => {
+    // POSTを受けつける
+    if (req.method !== 'POST') {
+      if (req.body && req.body.formData) {
+        req.body = JSON.parse(req.body.formData)
+      }
+      return app.render(req, res, req.url)
+    }
+    return handle(req, res)
+  })
 })
