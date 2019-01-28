5740af013550ce262ce044621679aac098858c15
佐藤一輝 <i.tennis0210@gmail.com>
2019-01-28 20:54:41 +0900

    firebase functionsのconfigとenvを紐づけ

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,6 +1,10 @@
 const functions = require('firebase-functions')
 const nextApp = require('./nextApp')
 
+process.env.HOST = functions.config().app.host
+process.env.API_HOST = functions.config().app.apihost
+process.env.PROXY_PATH = functions.config().app.proxypath
+
 exports.next = functions.https.onRequest((req, res) => {
   console.log('File: ' + req.originalUrl)
   return nextApp(req, res)
