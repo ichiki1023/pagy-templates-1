463d9cf39459219247a97634b44f49131c515faa
佐藤一輝 <i.tennis0210@gmail.com>
2019-01-29 13:53:21 +0900

    nextAppとindex.jsの統合

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,11 +1,48 @@
 const functions = require('firebase-functions')
-const nextApp = require('./nextApp')
+const express = require('express')
+const next = require('next')
+const bodyParser = require('body-parser')
 
 process.env.HOST = functions.config().app.host
 process.env.API_HOST = functions.config().app.apihost
 process.env.PROXY_PATH = functions.config().app.proxypath
 
-exports.next = functions.https.onRequest((req, res) => {
-  console.log('File: ' + req.originalUrl)
-  return nextApp(req, res)
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ conf: { distDir: 'next' }, dev })
+const handle = app.getRequestHandler()
+
+exports.next = functions.https.onRequest((request, response) => {
+  console.log('File: ' + request.originalUrl)
+  return app.prepare().then(() => {
+    const server = express()
+
+    server.use(bodyParser.json()) // for parsing application/json
+    server.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+    server.post('*', (req, res) => {
+      if (req.body && req.body.formData) {
+        req.body = JSON.parse(req.body.formData)
+      }
+      if (process.env.PROXY_PATH) {
+        req.url = req.url.replace(`${process.env.PROXY_PATH}`, '')
+      }
+      // ルートか判定
+      if (!req.url) {
+        req.url = '/'
+      }
+      app.render(req, res, req.url)
+    })
+
+    server.get('*', (req, res) => {
+      if (process.env.PROXY_PATH) {
+        req.url = req.url.replace(`${process.env.PROXY_PATH}`, '')
+      }
+      // ルートか判定
+      if (!req.url) {
+        req.url = '/'
+      }
+      return handle(req, res)
+    })
+    server(request, response)
+  })
 })
