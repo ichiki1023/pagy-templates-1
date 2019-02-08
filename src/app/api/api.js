05dbb001a6371187958329fd493b7ff115777f24
佐藤一輝 <i.tennis0210@gmail.com>
2019-02-08 21:10:54 +0900

    apiのlogger追加

--- src/app/api/api.js
+++ src/app/api/api.js
@@ -7,4 +7,27 @@ const apiHost = publicRuntimeConfig.apiHost
 axios.defaults.baseURL = `http://${apiHost}`
 axios.defaults.timeout = 5000
 
+// Add a request interceptor
+axios.interceptors.request.use(request => {
+  const { baseURL, url, method } = request
+  const params = request.params ? request.params : {}
+  const msg = `[Request] ${method.toUpperCase()}  ${baseURL}/${url} ${JSON.stringify(
+    params
+  )}`
+  console.debug(msg)
+  return request
+})
+
+// Add a response interceptor
+axios.interceptors.response.use(response => {
+  const {
+    status,
+    statusText,
+    config: { method, url }
+  } = response
+  const msg = `[Response] ${method.toUpperCase()} ${status}:${statusText} ${url}`
+  console.debug(msg)
+  return response
+})
+
 export default axios
