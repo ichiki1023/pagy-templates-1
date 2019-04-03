79d31e0d1a06a9507753b39e3b6010de994c24d3
佐藤一輝 <i.tennis0210@gmail.com>
2019-04-03 18:27:23 +0900

    withSiteを削除

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -4,6 +4,11 @@ import { getUserAgent } from 'app/helpers/userAgent'
 import { MuiThemeProvider } from '@material-ui/core/styles'
 import getPageContext from 'app/helpers/getPageContext'
 import { createGlobalStyle } from 'styled-components'
+import { getData } from 'app/helpers/data'
+import Error from 'next/error'
+
+// next.jsのバグ。_app.jsでcssを読み込まないとcssをimportしている画面へ遷移できない
+// https://spectrum.chat/next-js/general/bounty-for-issues~2183fc55-236d-42cb-92b9-3ab10acc6303?m=MTUzODU2NDg2ODA2Mg==
 import 'app/css/empty.css'
 
 const GlobalStyle = createGlobalStyle`
@@ -32,18 +37,36 @@ class TemplateApp extends App {
     const { req } = ctx
     let pageProps = {}
 
+    const hostName = req
+      ? req.headers['x-forwarded-host']
+      : window.location.hostname
+
     // userAgent取得
     const ua = req ? req.headers['user-agent'] : window.navigator.userAgent
     const userAgent = getUserAgent(ua)
 
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx)
-    }
+    try {
+      const data = await getData(ctx, hostName)
 
-    return {
-      pageProps: {
-        ...pageProps,
-        userAgent
+      if (Component.getInitialProps) {
+        pageProps = await Component.getInitialProps({ ...ctx, data: data })
+      }
+
+      return {
+        pageProps: {
+          ...data,
+          ...pageProps,
+          userAgent
+        },
+        hostName: hostName
+      }
+    } catch (error) {
+      console.debug(error)
+      return {
+        pageProps: {
+          ...pageProps,
+          statusCode: 500
+        }
       }
     }
   }
@@ -59,9 +82,23 @@ class TemplateApp extends App {
     if (jssStyles && jssStyles.parentNode) {
       jssStyles.parentNode.removeChild(jssStyles)
     }
+    const { site, fashion } = this.props.pageProps
+    window['__SITE_DATA__'] = {
+      site,
+      fashion
+    }
   }
 
   render () {
+    // エラー画面の表示
+    if (this.props.pageProps.statusCode) {
+      return (
+        <Container>
+          <Error statusCode={this.props.pageProps.statusCode} />
+        </Container>
+      )
+    }
+
     const { Component, pageProps } = this.props
     const { pageContext } = this
 
